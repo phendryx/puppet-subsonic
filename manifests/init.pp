b81f79863098dbeffd5970e30ff0779181d63a8a
Craig Watson <craig.watson@youdevise.com>
2012-05-25 14:26:42 +0100

    Moving config to init

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,17 @@
-class subsonic {
-  include subsonic::install
+define subsonic (
+                 $http_port = 'undef',
+                 $https_port = 'undef',
+                 $max_memory = '128M',
+                ){
+
   include subsonic::service
-  include subsonic::config
+  include subsonic::install
+
+
+  file { '/etc/default/subsonic':
+    owner   => 'root',
+    group   => 'root',
+    content => template('subsonic/default.erb'),
+    notify  => Service['subsonic'],
+  }
 }
